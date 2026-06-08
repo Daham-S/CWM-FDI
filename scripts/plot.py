b33a2ed8e2a5d1010450ddb7a7c3f472b9ed2b2d
DS <114287988+Daham-S@users.noreply.github.com>
2026-06-08 15:47:10 +0100

    Update plot parameters for CPU time analysis

--- scripts/plot.py
+++ scripts/plot.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 
 # parameters to modify 
-filename="my_input.txt"
+filename="time_py.txt"
 label='label'
 xlabel = 'Iterations'
 ylabel = 'Cpu Time Diff'
-title='Simple plot'
+title='CPU Time Diff over 1m iterations'
 fig_name='my_graph.png'
 bins=100 #adjust the number of bins to your plot
 
