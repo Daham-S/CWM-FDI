bdb73740a1c749715046a5e51dae026410ce1126
Daham Sooriyabandara <dahams006@gmail.com@git config --global user.email dahams006@gmail.comgit config --global user.email dahams006@gmail.com@>
2026-06-08 16:32:16 +0100

    plot the CDF function in python

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 
 # parameters to modify 
 filename="time_py.txt"
-label='label'
-xlabel = 'Iterations'
-ylabel = 'Cpu Time Diff'
-title='CPU Time Diff over 1m iterations'
+label='Python'
+xlabel = 'Time (ns)'
+ylabel = 'CDF (Prob of being less than T)'
+title='CDF of CPU Time Diff over 100  iterations'
 fig_name='my_graph.png'
 bins=100 #adjust the number of bins to your plot
 
 ## load data from input file
-t = np.loadtxt(filename, delimiter=" ", dtype="float")
+t = np.loadtxt(filename, delimiter=" ", dtype="float")[2:]
 
 ## if your data is "X Y" (2 cols), use the following line
 #plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
@@ -22,13 +22,13 @@ t = np.loadtxt(filename, delimiter=" ", dtype="float")
 #plt.plot(t, label=label)  # Plot some data on the (implicit) axes.
 
 ## comment the lines above and uncomment the line below to plot a simple CDF
-plt.hist(t[:], bins, density=True, histtype='step', cumulative=True, label=label)
+#plt.hist(t[:], bins, density=True, histtype='step', cumulative=True, label=label)
 
 ## comment the lines above and uncomment the 4 lines below for a nicer CDF
-#n = np.arange(1,len(t)+1) / float(len(t))
-#ts = np.sort(t)
-#fig, ax = plt.subplots()
-#ax.step(ts,n)
+n = np.arange(1,len(t)+1) / float(len(t))
+ts = np.sort(t)
+fig, ax = plt.subplots()
+ax.step(ts,n)
 
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
