fbd3c27d9c34f98a68ed8956dc95626170efd230
Daham Sooriyabandara <dahams006@gmail.com@git config --global user.email dahams006@gmail.comgit config --global user.email dahams006@gmail.com@>
2026-06-09 10:55:53 +0100

    Ping logs

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -4,16 +4,17 @@ import matplotlib.pyplot as plt
 
 
 # parameters to modify 
-filename="time_c.txt"
+filename="ping_1.log"
 label='Python'
 xlabel = 'Time (ns)'
 ylabel = 'CDF (Prob of being less than T)'
-title='CDF of CPU Time Diff over 1m iterations'
+title='CDF of Ping RTT over 1000 iterations, interval 0.01'
 fig_name='my_graph.png'
 bins=100 #adjust the number of bins to your plot
 
 ## load data from input file
 t = np.loadtxt(filename, delimiter=" ", dtype="float")[0:]
+
 
 ## if your data is "X Y" (2 cols), use the following line
 #plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
@@ -29,6 +30,27 @@ n = np.arange(1,len(t)+1) / float(len(t))
 ts = np.sort(t)
 fig, ax = plt.subplots()
 ax.step(ts,n)
+
+# important values
+min = ts[0]
+max = ts[-1]
+mean = np.mean(ts)
+median = np.median(ts)
+percentile_90 = np.percentile(ts,90)
+percentile_99 = np.percentile(ts,99)
+
+print(f"Mean is {mean: .2f}")
+print(f"Median is {median: .2f}")
+print(f"90th Percentile is {percentile_90: .2f}")
+print(f"99th Percentile is {percentile_99: .2f}")
+print(f"Maximum is {max: .2f}")
+print(f"Minimum is {min: .2f}")
+
+
+
+
+
+
 
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
